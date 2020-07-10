ea4bafda77a80a0fbd33937c8106edb44252b74b
karimkhattaby <18755153+karimkhattaby@users.noreply.github.com>
2020-07-10 12:46:28 +0400

    Add Search Box Component

---
+++ src/SearchBox.js
@@ -0,0 +1,17 @@
+// @ts-nocheck
+import React from 'react';
+
+const SearchBox = ({ searchChange }) => {
+    return (
+        <div className='pa2'>
+            <input
+            className='pa3 ba b--green bg-lightest-blue'
+            type="search"
+            placeholder='search Robots...'
+            onChange={searchChange}
+            />
+        </div>
+    );
+};
+
+export default SearchBox;
