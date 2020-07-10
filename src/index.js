55af8466be7b8f95d69acbbac48264dc804f22d9
karimkhattaby <18755153+karimkhattaby@users.noreply.github.com>
2020-07-10 11:24:49 +0400

    Add index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
+// @ts-nocheck
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <h1>Hello World</h1>,
   document.getElementById('root')
 );
 
